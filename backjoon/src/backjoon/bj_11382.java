fccc453458dc8287d8d3f9d080db73eb95bb5b20
sleepysudal <kyjyoung3997@naver.com>
2024-01-16 04:22:56 +0900

    11382

---
+++ backjoon/src/backjoon/bj_11382.java
@@ -0,0 +1,21 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class bj_11382 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		//System.out.println("정수 3개를 입력해주세요");
+		long a = sc.nextLong();  
+        long b = sc.nextLong(); 
+        long c = sc.nextLong();  
+        long d = a + b + c;
+        //System.out.println("정수 3개를 합친 값은");
+        System.out.println(d); 
+        
+	}
+
+}
